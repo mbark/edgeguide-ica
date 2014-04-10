99cee5f39c7448339f4c17082d72982dfb277239
mbark <barksten@kth.se>
2014-04-10 17:00:41 +0200

    login bugfix

--- apps/TIIT/common/js/login.js
+++ apps/TIIT/common/js/login.js
@@ -5,12 +5,14 @@ currentPage = {};
 
 currentPage.init = function() {
 	currentPage.loadPage();
+	$('#footer').html('<a href="#" data-role="button" id="login" onclick="currentPage.loadPage(\'events.html\');">Login with facebook</a>');
+ 	$('#login').button();
 }
 
 currentPage.loadPage = function(page){
-	console.log('hej');
-	$('#footer').html('<a href="#" data-role="button" id="login" onclick="currentPage.loadPage(\'events.html\');">Login with facebook</a>');
-	$('#login').button();
-
+	pagesHistory.push("login.html");
+	$("#main").load(page, function(){
+		currentPage.init();
+	 });
 };
 
