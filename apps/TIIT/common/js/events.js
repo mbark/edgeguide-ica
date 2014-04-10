880970f5224f50ee6516cedd246960333bbcaa25
mbark <barksten@kth.se>
2014-04-10 16:29:33 +0200

    Make lists work with jquery mobile

--- apps/TIIT/common/js/events.js
+++ apps/TIIT/common/js/events.js
@@ -10,14 +10,14 @@ currentPage.init = function() {
 	events.push({
 		name : "Studs event"
 	});
-	
+
 	addEventsToList(events);
 };
 
 function addEventsToList(events) {
 	$.each(events, function(i, event) {
-		console.log("Adding event to list " + event);
-		$('#event-list').append('<li><a href="#">' + event.name + '</a>' + '</li>');
+		$('#event-list').append(
+				'<li><a href="#">' + event.name + '</a>' + '</li>');
 	});
-	$("#event-list").listview("refresh");
+	$("#event-list").listview().listview("refresh");
 }
