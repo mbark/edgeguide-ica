959b0f80fb515de851a06a31d79f202872a7b0d0
Per Classon <perwclasson@gmail.com>
2014-04-10 17:49:28 +0200

    Style changes

--- apps/TIIT/common/js/events.js
+++ apps/TIIT/common/js/events.js
@@ -24,8 +24,7 @@ var fontAwesomeTrashBin = '<i class="fa fa-trash-o delete-event-icon">';
 function addEventsToList(events) {
 	$.each(events, function(i, event) {
 		$('#event-list').append(
-				'<li data-icon="false"><a href="#">' + event.name
-						+ fontAwesomeTrashBin + '</a>' + '</li>');
+				'<li data-icon="false"><a href="#">' + event.name + '</a>' + fontAwesomeTrashBin + '</li>');
 	});
 	$("#event-list").listview();
 }
